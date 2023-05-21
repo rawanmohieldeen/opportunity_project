e3ff62e93a2aa47ed24f4d7d75b0f121ff85b74c
rawanmohieldeen <rawanmohieldeen95@gmail.com>
2023-05-21 03:12:17 +0200

    translation

--- hrms-sa/business_request/models/models.py
+++ hrms-sa/business_request/models/models.py
@@ -85,7 +85,7 @@ class BusinessTravelRequest(models.Model):
         if self.state == 'approved':
             raise UserError('You cannot delete approved request')
         else:
-            return super().unlink(self)
+            return super().unlink()
     def submit(self):
         self.state = 'review'
     def review(self):
